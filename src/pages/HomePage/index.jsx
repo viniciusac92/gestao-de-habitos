d4bae0a70b98f5e8718e4f9b4080856d52b1d96b
vinicius <vinicius.campello@hotmail.com>
2021-03-11 22:48:41 -0300

    WIP: Resolução de conflitos Merge.

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,17 +1,16 @@
 import Home from "../../components/Home";
-import { motion } from "framer-motion";
+import {motion} from "framer-motion";
 
 const HomePage = () => {
-  return (
-    <motion.div
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      exit={{ opacity: 0 }}
-      transition={{ duration: 1 }}
-    >
-      <Home />
-    </motion.div>
-  );
+	return (
+		<motion.div
+			initial={{opacity: 0}}
+			animate={{opacity: 1}}
+			exit={{opacity: 0}}
+			transition={{duration: 1}}>
+			<Home />
+		</motion.div>
+	);
 };
 
 export default HomePage;
