2019736516a08d411c1aafd8b36e2f27018c6b33
VictorSoares <cvictorsda@gmail.com>
2021-03-13 13:46:13 -0300

    feat: added trail graph

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -1,30 +1,34 @@
-import {Switch, Route} from "react-router-dom";
-import {AnimatePresence} from "framer-motion";
+import { Switch, Route } from "react-router-dom";
+import { AnimatePresence } from "framer-motion";
 import LoginPage from "../pages/LoginPage";
 // import Usuarios from "../components/Usuarios";
 import RegisterPage from "../pages/RegisterPage";
 import UserPerfilPage from "../pages/UserPerfilPage";
 import HomePage from "../pages/HomePage";
+import TrailPage from "../pages/TrailPage";
 
 const Routes = () => {
-	return (
-		<AnimatePresence exitBeforeEnter>
-			<Switch>
-				<Route exact path="/">
-					<LoginPage />
-				</Route>
-				<Route path="/home">
-					<HomePage />
-				</Route>
-				<Route path="/register">
-					<RegisterPage />
-				</Route>
-				<Route path="/profile">
-					<UserPerfilPage />
-				</Route>
-			</Switch>
-		</AnimatePresence>
-	);
+  return (
+    <AnimatePresence exitBeforeEnter>
+      <Switch>
+        <Route exact path="/">
+          <LoginPage />
+        </Route>
+        <Route path="/home">
+          <HomePage />
+        </Route>
+        <Route path="/register">
+          <RegisterPage />
+        </Route>
+        <Route path="/profile">
+          <UserPerfilPage />
+        </Route>
+        <Route path="/trail">
+          <TrailPage />
+        </Route>
+      </Switch>
+    </AnimatePresence>
+  );
 };
 
 export default Routes;
