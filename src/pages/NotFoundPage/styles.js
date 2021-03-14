d48ffd2c99b30e97a0d6f10b8b387b6b9fb8b03a
matheus ribeiro <tico.ribeiro0396@gmail.com>
2021-03-14 13:00:15 -0300

    feat: adds NotFound Page

---
+++ src/pages/NotFoundPage/styles.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const ErrorMsgDiv = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-image: url("https://image.freepik.com/free-vector/404-error-page-found-illustration_199563-1.jpg");
+  background-repeat: repeat;
+`;
