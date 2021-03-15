b21e8fe872ad9bf3399615654ead789456974ef0
vinicius <vinicius.campello@hotmail.com>
2021-03-15 14:58:12 -0300

    WIP: Feito o map para gerar a página de grupos.

--- src/Providers/Group/index.jsx
+++ src/Providers/Group/index.jsx
@@ -1,21 +1,28 @@
-import { createContext, useContext, useState } from "react";
-import api from '../../Services'
+import {useEffect} from "react";
+import {createContext, useContext, useState} from "react";
+import api from "../../Services";
 
 const GroupContext = createContext();
 
-export const GroupProvider = ({ children }) => {
+export const GroupProvider = ({children}) => {
+	const [group, setGroup] = useState(() => {
+		api
+			.get("/groups/26/")
+			.then((response) => setGroup(response.data))
+			.catch((err) => console.log(err));
+	});
 
-  const [group, setGroup] = useState(()=> {
-    api.get("/groups/26/")
-      .then((response) => setGroup(response.data))
-      .catch((err) => console.log(err))
-  })
+	// useEffect(()=>{
+	//   api.get("/groups/26/")
+	//     .then((response) => setGroup(response.data))
+	//     .catch((err) => console.log(err))
+	// },[])
 
-  return (
-    <GroupContext.Provider value={{ group, setGroup }}>
-      {children}
-    </GroupContext.Provider>
-  );
+	return (
+		<GroupContext.Provider value={{group, setGroup}}>
+			{children}
+		</GroupContext.Provider>
+	);
 };
 
 export const useGroup = () => useContext(GroupContext);
--- src/components/Groups/index.jsx
+++ src/components/Groups/index.jsx
@@ -1,38 +1,24 @@
-import Accordion from "@material-ui/core/Accordion";
-import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
-import {makeStyles} from "@material-ui/core/styles";
 import {
 	DivItems,
 	DivPerfil,
-	AccordionSummaryStyled,
 	BoxStyled,
 	TypographyStyled,
-	DivStyled,
 	CardUser,
 	ListItemStyled,
-	ListItemTextStyled,
 	DividerStyled,
 } from "./styles";
-import {
-	Box,
-	LinearProgress,
-	Typography,
-	List,
-	ListItem,
-	ListItemText,
-	Divider,
-} from "@material-ui/core";
+import {Box, LinearProgress, List} from "@material-ui/core";
 import {Avatar} from "@material-ui/core";
 import {Card} from "@material-ui/core";
-import {useHabits} from "../../Providers/Habits";
 import {useGroup} from "../../Providers/Group";
+import {useProviderUser} from "../../Providers/User";
 
 const Groups = (props) => {
 	const {group} = useGroup();
+	const {userName} = useProviderUser();
 
 	console.log(group);
-	console.log(group["id"]);
-
+	console.log(group?.users);
 	return (
 		<DivItems>
 			<DivPerfil>
@@ -47,91 +33,32 @@ const Groups = (props) => {
 					<div style={CardUser.topProfile}>
 						<Avatar variant="rounded" style={CardUser.avatar} />
 					</div>
-					<h3 style={CardUser.userName}>username</h3>
-					{/* <Typography variant="h2" component="h2">
-						Username
-					</Typography> */}
+					<h3 style={CardUser.userName}>{userName}</h3>
 				</Card>
 			</DivPerfil>
-			{/* {group.users.map((userProfile, index) => {
-				<div key={index}>
-					<List>
-						<ListItemStyled>
-							<TypographyStyled>{userProfile.username}</TypographyStyled>
-							<BoxStyled display="flex" alignItems="center">
-								<Box width="100%" mr={1}>
-									<LinearProgress variant="determinate" {...props} />
-								</Box>
-								<Box minWidth={35}>
-									<TypographyStyled variant="body2">78 Pontos</TypographyStyled>
-								</Box>
-							</BoxStyled>
-							<TypographyStyled>Lorem Ipsum</TypographyStyled>
-						</ListItemStyled>
-					</List>
-					<DividerStyled />
-				</div>;
-			})} */}
-			<List>
-				<ListItemStyled>
-					{/* <TypographyStyled>{group.users[0].username}</TypographyStyled> */}
-					<BoxStyled display="flex" alignItems="center">
-						<Box width="100%" mr={1}>
-							<LinearProgress variant="determinate" {...props} />
-						</Box>
-						<Box minWidth={35}>
-							<TypographyStyled variant="body2">78 Pontos</TypographyStyled>
-						</Box>
-					</BoxStyled>
-					<TypographyStyled>Lorem Ipsum</TypographyStyled>
-				</ListItemStyled>
-			</List>
-			<DividerStyled />
-			<List>
-				<ListItemStyled>
-					{/* <TypographyStyled>{group.users[0].username}</TypographyStyled> */}
-					<BoxStyled display="flex" alignItems="center">
-						<Box width="100%" mr={1}>
-							<LinearProgress variant="determinate" {...props} />
-						</Box>
-						<Box minWidth={35}>
-							<TypographyStyled variant="body2">78 Pontos</TypographyStyled>
-						</Box>
-					</BoxStyled>
-					<TypographyStyled>Lorem Ipsum</TypographyStyled>
-				</ListItemStyled>
-			</List>
-			<DividerStyled />
-			<List>
-				<ListItemStyled>
-					{/* <TypographyStyled>{group.users[0].username}</TypographyStyled> */}
-					<BoxStyled display="flex" alignItems="center">
-						<Box width="100%" mr={1}>
-							<LinearProgress variant="determinate" {...props} />
-						</Box>
-						<Box minWidth={35}>
-							<TypographyStyled variant="body2">78 Pontos</TypographyStyled>
-						</Box>
-					</BoxStyled>
-					<TypographyStyled>Lorem Ipsum</TypographyStyled>
-				</ListItemStyled>
-			</List>
-			<DividerStyled />
-			<List>
-				<ListItemStyled>
-					{/* <TypographyStyled>{group.users[0].username}</TypographyStyled> */}
-					<BoxStyled display="flex" alignItems="center">
-						<Box width="100%" mr={1}>
-							<LinearProgress variant="determinate" {...props} />
-						</Box>
-						<Box minWidth={35}>
-							<TypographyStyled variant="body2">78 Pontos</TypographyStyled>
-						</Box>
-					</BoxStyled>
-					<TypographyStyled>Lorem Ipsum</TypographyStyled>
-				</ListItemStyled>
-			</List>
-			<DividerStyled />
+			{group?.users.map((userProfile, index) => {
+				return (
+					<div key={index}>
+						<List>
+							<ListItemStyled>
+								<TypographyStyled>{userProfile.username}</TypographyStyled>
+								<BoxStyled display="flex" alignItems="center">
+									<Box width="100%" mr={1}>
+										<LinearProgress variant="determinate" {...props} />
+									</Box>
+									<Box minWidth={35}>
+										<TypographyStyled variant="body2">
+											{`${userProfile.id} XP`}
+										</TypographyStyled>
+									</Box>
+								</BoxStyled>
+								<TypographyStyled>{`Text me! ${userProfile.email}`}</TypographyStyled>
+							</ListItemStyled>
+						</List>
+						<DividerStyled />
+					</div>
+				);
+			})}
 		</DivItems>
 	);
 };
