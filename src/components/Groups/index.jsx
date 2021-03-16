15eeeb7ce7deca711adde13d8d76f6f972488086
vinicius <vinicius.campello@hotmail.com>
2021-03-15 22:01:50 -0300

    WIP: Feita a ordenação do ranking.

--- src/components/Groups/index.jsx
+++ src/components/Groups/index.jsx
@@ -8,20 +8,15 @@ import {
 	DividerStyled,
 	LinearProgressStyled,
 } from "./styles";
-import {Box, LinearProgress, List} from "@material-ui/core";
+import {Box, List} from "@material-ui/core";
 import {Avatar} from "@material-ui/core";
 import {Card} from "@material-ui/core";
 import {useGroup} from "../../Providers/Group";
 import {useProviderUser} from "../../Providers/User";
-import {useEffect} from "react";
-import {useState} from "react";
 
 const Groups = () => {
 	const {group} = useGroup();
 	const {userName} = useProviderUser();
-
-	console.log(group);
-	console.log(group && Object.entries(group.users).sort());
 
 	return (
 		<DivItems>
@@ -40,35 +35,38 @@ const Groups = () => {
 					<h3 style={CardUser.userName}>{userName}</h3>
 				</Card>
 			</DivPerfil>
-			{group?.users.map((userProfile, index) => {
-				return (
-					<div key={index}>
-						<List>
-							<ListItemStyled>
-								<TypographyStyled>{`${userProfile.username} #${
-									index + 1
-								}`}</TypographyStyled>
-								<BoxStyled display="flex" alignItems="center">
-									<Box width="150%" mr={1}>
-										<LinearProgressStyled
-											variant="determinate"
-											color="primary"
-											{...{value: `${group?.users[index].group}`}}
-										/>
-									</Box>
-									<Box minWidth={35}>
-										<TypographyStyled variant="body2">
-											{`${group?.users[index].group}%`}
-										</TypographyStyled>
-									</Box>
-								</BoxStyled>
-								<TypographyStyled>{`Text me! ${userProfile.email}`}</TypographyStyled>
-							</ListItemStyled>
-						</List>
-						<DividerStyled />
-					</div>
-				);
-			})}
+			{group &&
+				group.users
+					.sort((a, b) => (a.id > b.id ? -1 : 1))
+					.map((userProfile, index) => {
+						return (
+							<div key={index}>
+								<List>
+									<ListItemStyled>
+										<TypographyStyled>{`${userProfile.username} #${
+											index + 1
+										}`}</TypographyStyled>
+										<BoxStyled display="flex" alignItems="center">
+											<Box width="150%" mr={1}>
+												<LinearProgressStyled
+													variant="determinate"
+													color="primary"
+													{...{value: `${group?.users[index].id}`}}
+												/>
+											</Box>
+											<Box minWidth={35}>
+												<TypographyStyled variant="body2">
+													{`${group?.users[index].id}%`}
+												</TypographyStyled>
+											</Box>
+										</BoxStyled>
+										<TypographyStyled>{`Text me! ${userProfile.email}`}</TypographyStyled>
+									</ListItemStyled>
+								</List>
+								<DividerStyled />
+							</div>
+						);
+					})}
 		</DivItems>
 	);
 };
