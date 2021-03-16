8fb11aa7e5fb2b6e1652ed04089b4447965a9a58
VictorSoares <cvictorsda@gmail.com>
2021-03-16 10:55:51 -0300

    feat: styled profile buttons

--- src/Routes/index.jsx
+++ src/Routes/index.jsx
@@ -1,5 +1,5 @@
-import {Switch} from "react-router-dom";
-import {AnimatePresence} from "framer-motion";
+import { Switch } from "react-router-dom";
+import { AnimatePresence } from "framer-motion";
 import LoginPage from "../pages/LoginPage";
 import RegisterPage from "../pages/RegisterPage";
 import UserPerfilPage from "../pages/UserPerfilPage";
@@ -8,24 +8,23 @@ import TrailPage from "../pages/TrailPage";
 import GroupsPage from "../pages/GroupsPage";
 import Route from "./route";
 import NotFound from "../pages/NotFoundPage";
-import ContadorPage from "../pages/ContadorPage";
 
 const Routes = () => {
-	return (
-		<AnimatePresence exitBeforeEnter>
-			<Switch>
-				<Route exact path="/" component={LoginPage} />
-				<Route path="/home" component={HomePage} isPrivate />
-				<Route path="/register" component={RegisterPage} />
-				<Route path="/profile" component={UserPerfilPage} isPrivate />
-				<Route path="/groups" component={GroupsPage} isPrivate />
-				<Route path="/trail" component={TrailPage} isPrivate>
-					<TrailPage />
-				</Route>
-				<Route component={NotFound} />
-			</Switch>
-		</AnimatePresence>
-	);
+  return (
+    <AnimatePresence exitBeforeEnter>
+      <Switch>
+        <Route exact path="/" component={LoginPage} />
+        <Route path="/home" component={HomePage} isPrivate />
+        <Route path="/register" component={RegisterPage} />
+        <Route path="/profile" component={UserPerfilPage} isPrivate />
+        <Route path="/groups" component={GroupsPage} isPrivate />
+        <Route path="/trail" component={TrailPage} isPrivate>
+          <TrailPage />
+        </Route>
+        <Route component={NotFound} />
+      </Switch>
+    </AnimatePresence>
+  );
 };
 
 export default Routes;
